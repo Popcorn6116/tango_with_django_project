88cc0ec7902bb5728cfb3c0906163c8a8ec46417
Haojun Ma <2549410m@student.gla.ac.uk>
2021-07-28 21:51:46 +0800

    finish chapter5, and add .gitignore.txt to remote

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('', views.about, name='about'),
     path('rango/', include('rango.urls')),
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
